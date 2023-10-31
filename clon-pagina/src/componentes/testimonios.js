9d0be43b2cc7e7c325e3c01fc85b3856c965fa9b
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2023-10-30 22:35:27 -0500

    Clon testimonio terminado

--- clon-pagina/src/componentes/testimonios.js
+++ clon-pagina/src/componentes/testimonios.js
@@ -1,13 +1,21 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import '../hojas-de-estilo.css/testimonios.css';
 
-function Testimonio(){
+
+function Testimonio(props){
   return(
     <div className='contenedor-testimonio'>
-      <img className='imagen.testimonio'
-      src={require('../imagenes/Imagen 1.jpg')}
+      <img className='imagen-testimonio'
+      src={require(`../imagenes/Imagen ${props.imagen}.jpg`)}
       alt='Foto 1 para testimonio'/>
+
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
     </div>
   );
 }
+
+export default Testimonio;
